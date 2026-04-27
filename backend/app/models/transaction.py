56eafc96bdabdd878657108fc1e814a734ea7d7d
Swati Jain <98042616+Swati-jain123@users.noreply.github.com>
2026-04-27 11:41:36 +0530

    Update transaction.py

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -15,33 +15,12 @@ class Transaction(models.Model):
     reference_id = models.CharField(max_length=255, unique=True, db_index=True)
     metadata = models.JSONField(default=dict)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        is_new = self.pk is None
-        super().save(*args, **kwargs)
-
-        if is_new:
-            merchant = self.merchant
-
-            if self.transaction_type == "CREDIT":
-                merchant.balance += self.amount_paise
-
-            elif self.transaction_type == "DEBIT":
-                merchant.balance -= self.amount_paise
-
-            elif self.transaction_type == "HOLD":
-                merchant.balance -= self.amount_paise
-
-            elif self.transaction_type == "RELEASE":
-                merchant.balance += self.amount_paise
-
-            merchant.save()
-
+    
     class Meta:
         indexes = [
             models.Index(fields=['merchant', 'created_at']),
             models.Index(fields=['reference_id']),
         ]
-
+    
     def __str__(self):
         return f"{self.transaction_type} - {self.amount_paise} paise - {self.merchant.business_name}"
